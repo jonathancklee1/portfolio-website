7de2033973be2783cfc74afdbfec2f5b4e59fc6f
jonathancklee1 <jonathancklee@gmail.com>
2022-05-18 21:24:07 +1000

    Implemented mobile nav bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./styles/style.scss";
 import SideNav from "./components/SideNav";
+import MobileHeader from "./components/MobileHeader";
 
 function App() {
   return (
     <div className="App">
       <SideNav />
+      <MobileHeader />
       <main>
+        <section className="about-me">
+          <div className="about-me-text__wrapper">
+            <h1>Hi, I'm Jonathan Lee</h1>
+            <h2>Information Systems Graduate</h2>
+            <p>Welcome to my portfolio website!</p>
+          </div>
+          {/* <button className="resume-btn">Resume</button> */}
+        </section>
         <section className="about-me">
           <h1>Hi, I'm Jonathan Lee</h1>
         </section>
---
+++ src/components/MobileHeader.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUser,
+  faLaptop,
+  faScrewdriverWrench,
+  faEnvelope,
+  faTimes,
+  faBars,
+} from "@fortawesome/free-solid-svg-icons";
+
+export default function MobileHeader() {
+  const [navOpen, setNavOpen] = React.useState(false);
+
+  function toggleNav() {
+    setNavOpen((prevNavOpen) => !prevNavOpen);
+  }
+
+  return (
+    <header className="mobile-header">
+      <h1 className="mobile-logo">&lt;JCKL&gt;</h1>
+      {navOpen ? (
+        <FontAwesomeIcon
+          className="hamburger-icon"
+          icon={faBars}
+          onClick={toggleNav}
+        />
+      ) : (
+        <FontAwesomeIcon
+          className="close-icon"
+          icon={faTimes}
+          onClick={toggleNav}
+        />
+      )}
+      <div
+        className={
+          navOpen ? "mobile-nav-container open" : "mobile-nav-container"
+        }
+      >
+        <nav className="mobile-nav">
+          <ul>
+            <li onClick={() => setNavOpen(false)}>
+              <div className="mobile-nav-btn">
+                <FontAwesomeIcon icon={faUser} color="#fff" />
+              </div>
+              <p className="mobile-nav-btn-text">About Me</p>
+            </li>
+            <li onClick={() => setNavOpen(false)}>
+              <div className="mobile-nav-btn">
+                <FontAwesomeIcon icon={faLaptop} color="#fff" />
+              </div>
+              <p className="mobile-nav-btn-text">Projects</p>
+            </li>
+            <li onClick={() => setNavOpen(false)}>
+              <div className="mobile-nav-btn">
+                <FontAwesomeIcon icon={faScrewdriverWrench} color="#fff" />
+              </div>
+              <p className="mobile-nav-btn-text">Skills</p>
+            </li>
+            <li onClick={() => setNavOpen(false)}>
+              <div className="mobile-nav-btn">
+                <FontAwesomeIcon icon={faEnvelope} color="#fff" />
+              </div>
+              <p className="mobile-nav-btn-text">Contact Me</p>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
